4cbe7cdf4fed015b5f68d96051656c918b0faaa9
tmaulon <thomas.maulon@gmail.com>
2019-12-03 16:21:46 +0100

    improve testClient

--- src/test/java/com/hello/models/TestClient.java
+++ src/test/java/com/hello/models/TestClient.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ClientTest {
+public class TestClient {
 
     @Test
     public void testClient()
